cb4eaa17fa60d80227474d31679bf9cb3ec0a893
albarkel@umich.edu <albarkel@umich.med.edu>
2021-10-13 13:13:03 -0400

    move footer copy to match header

--- src/components/NavFooter/atlas.js
+++ src/components/NavFooter/atlas.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class AtlasNavFooter extends Component {
   render() {
     return (
-      <div id="footer" className="fixed-bottom px-1 py-1">
+      <div id="footer" className="fixed-bottom px-3 py-1">
         <a
           className="text-light small"
           href="https://kpmp.org"
