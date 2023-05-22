d0ee8d4b9ad9f0709dce071f6a8556f172df0235
HaneenT <haneen@umich.edu>
2023-05-22 12:30:45 -0400

    KPMP-4461: Updated universal footer with new text.

--- src/components/NavFooter/atlas.js
+++ src/components/NavFooter/atlas.js
@@ -3,15 +3,8 @@ import React, { Component } from 'react';
 class AtlasNavFooter extends Component {
   render() {
     return (
-      <div id="footer" className="fixed-bottom px-3 py-1">
-        <a
-          className="text-light small"
-          href="https://kpmp.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          &copy; Kidney Precision Medicine Project
-        </a>
+      <div id="footer" className="fixed-bottom px-3 py-1 text-light small">
+        The Kidney Precision Medicine Project (KPMP) is supported by grant U24 DK114886 from the National Institute of Diabetes and Digestive and Kidney Diseases (NIDDK). It is a multi-site collaboration comprised of patients, clinicians, and investigators from across the United States. This website and its content are the property of KPMP. No part of this website may be reproduced, distributed, or transmitted in any form without the prior written permission of KPMP. For permission requests, please contact us at info@kpmp.org. 
       </div>
     );
   }
