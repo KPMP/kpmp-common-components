b0a8c45f6bea8c6dd5ca8c7d689393b5af123b14
HaneenT <haneen@umich.edu>
2023-05-23 10:24:36 -0400

    KPMP-4461: Added links to NIDDK and email.

--- src/components/NavFooter/atlas.js
+++ src/components/NavFooter/atlas.js
@@ -4,7 +4,12 @@ class AtlasNavFooter extends Component {
   render() {
     return (
       <div id="footer" className="fixed-bottom px-3 py-1 text-light small">
-        The Kidney Precision Medicine Project (KPMP) is supported by grant U24 DK114886 from the National Institute of Diabetes and Digestive and Kidney Diseases (NIDDK). It is a multi-site collaboration comprised of patients, clinicians, and investigators from across the United States. This website and its content are the property of KPMP. No part of this website may be reproduced, distributed, or transmitted in any form without the prior written permission of KPMP. For permission requests, please contact us at info@kpmp.org. 
+        The Kidney Precision Medicine Project (KPMP) is supported by grant U24 DK114886 from the <a
+          className='text-light'
+          href="https://www.niddk.nih.gov"
+          target="_blank"
+          rel="noopener noreferrer"
+        >National Institute of Diabetes and Digestive and Kidney Diseases</a> (NIDDK). It is a multi-site collaboration comprised of patients, clinicians, and investigators from across the United States. This website and its content are the property of KPMP. No part of this website may be reproduced, distributed, or transmitted in any form without the prior written permission of KPMP. For permission requests, please contact us at <a className='text-light' href="mailto:info@kpmp.org?subject=Request%20to%20use%20KPMP%20content">info@kpmp.org</a>. 
       </div>
     );
   }
